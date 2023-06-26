fd3f7cb05a208444a3260f8aab8b9660f006ee50
kf12345 <klintfaber@gmail.com>
2023-06-26 19:36:03 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,1 @@
+const spotifyAuth = require("./spotifyAuth.js");
---
+++ spotifyAuth.js
@@ -0,0 +1,26 @@
+const clientId = process.env['SPOTIFY_CLIENT_ID']; // Replace with your client ID
+const code = undefined;
+
+if (!code) {
+    redirectToAuthCodeFlow(clientId);
+} else {
+    const accessToken = await getAccessToken(clientId, code);
+    const profile = await fetchProfile(accessToken);
+    populateUI(profile);
+}
+
+async function redirectToAuthCodeFlow(clientId) {
+    // TODO: Redirect to Spotify authorization page
+}
+
+async function getAccessToken(clientId, code) {
+  // TODO: Get access token for code
+}
+
+async function fetchProfile(token) {
+    // TODO: Call Web API
+}
+
+function populateUI(profile) {
+    // TODO: Update UI with profile data
+}
