12f1cafbcb308dd90b3bff86d132eefb9493dc34
k-faber <klintfaber@gmail.com>
2023-06-27 19:20:19 +0000

    Readded spotify-web-api-node and axios. Made Spotify API stuff work.
    Readded axios and spotify-web-api-node. Can now make calls to spotify API through spotify-web-api-node and made token generation work with axios. Starting to work on GPT trials soon.

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-const spotifyAuth = require("./spotifyAuth.js");
+require('dotenv').config();
 
+const spotifyAuth = require('./src/spotifyAuth.js');
+const spotifyNode = require('spotify-web-api-node');
+
+var spotifyApi = new spotifyNode({
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  redirectUri: 'localhost:8080'
+});
+
+async function main() { // main made async so that i can get access token :)
+
+  const accessToken = await spotifyAuth.getAccessToken(); // get access code
+  spotifyApi.setAccessToken(accessToken);  // send to spotify API
+
+  // Get Muse's albums
+  spotifyApi.getArtistAlbums('12Chz98pHFMPJEknJQMWvI').then(
+    function(data) {
+      console.log('Artist albums', data.body);
+    },
+    function(err) {
+      console.error(err);
+    }
+  );
+
+}
+
+main();
 
 // WHAT TO DO:
 
